563ccefa1582a943a222afb39dc89914c7e0afc1
Jordan <jordan@jpdesigning.com>
2022-10-14 11:07:40 -0400

    starting point

--- apps/sandwich/cypress.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'cypress';
-import { nxComponentTestingPreset } from '@nrwl/react/plugins/component-testing';
-
-export default defineConfig({
-  component: nxComponentTestingPreset(__filename),
-});
